ffeb03aa2877fef08150c825d57046594c622f05
JuanjoMendez1 <juanjomendez029@gmail.com>
2024-02-06 21:42:03 -0600

    Se modifica codigo para envio de email

--- formulario.php
+++ formulario.php
@@ -1,25 +1,33 @@
 <?php
-// var_dump("Entropy");
-$nombre = $_POST['nombre'];
-$telefono = $_POST['telefono'];
-$email = $_POST['correo'];
-$mensaje = $_POST['mensaje']; 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nombre = $_POST['nombre'];
+    $telefono = $_POST['telefono'];
+    $email = $_POST['correo'];
+    $mensaje = $_POST['mensaje'];
 
     $email_to = "juanjomendez029@gmail.com";
-    $asunto = "Solicitud de Informacion Nova Devs";
-    $asunto_cliente="Gracias por enviar su informacion" ;
+    $asunto = "Solicitud de Información Nova Devs";
+    $asunto_cliente = "Gracias por enviar su información";
 
-    $comentario = "
-        Nombre del cliente: $_POST[nombre]
-        Email del cliente: $_POST[email]
-        Telefono del cliente: $_POST[telefono]
-        Mensaje o comentario: $_POST[mensaje]
-    ";
+    // Construcción del comentario
+    $comentario = "Nombre del cliente: $nombre\nEmail del cliente: $email\nTeléfono del cliente: $telefono\nMensaje o comentario: $mensaje";
 
-    $headers = 'From: '.$email."\r\n". 
-'Reply-To: '.$email_to."\r\n" .
-'X-Mailer: PHP/' . phpversion();
+    // Validación y limpieza de datos para evitar inyección de encabezados
+    $email_to = filter_var($email_to, FILTER_SANITIZE_EMAIL);
+    $asunto = filter_var($asunto, FILTER_SANITIZE_STRING);
+    $asunto_cliente = filter_var($asunto_cliente, FILTER_SANITIZE_STRING);
+    $comentario = wordwrap($comentario, 70, "\r\n");
 
-        mail($email_to,$asunto,$comentario,$headers);
-//         include 'index.html';
+    // Construcción de los encabezados
+    $headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email_to . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+    // Intento de envío del correo
+    if (mail($email_to, $asunto, $comentario, $headers)) {
+        echo "Correo enviado con éxito";
+    } else {
+        echo "Error al enviar el correo";
+    }
+}
 ?>
